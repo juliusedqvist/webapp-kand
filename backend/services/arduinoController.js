6ff235422281afdc86d11962a66b9a2e12711512
Julius Edqvist <jedqvist09@gmail.com>
2025-04-24 13:10:57 +0200

    Double arduino

--- backend/services/arduinoController.js
+++ backend/services/arduinoController.js
@@ -1,43 +1,72 @@
 const { SerialPort } = require('serialport');
 const { ReadlineParser } = require('@serialport/parser-readline');
 
-let port;
+let port1;
+let port2;
 let parser;
 let isConnected = false;
 
 // Create and open the serial port ONCE
-port = new SerialPort({ path: '/dev/ttyUSB0', baudRate: 9600 });
+port1 = new SerialPort({ path: '/dev/ttyUSB0', baudRate: 9600 });
+port2 = new SerialPort({ path: '/dev/ttyUSB1', baudRate: 9600 });
 
-port.on('open', () => {
+port1.on('open', () => {
     isConnected = true;
     console.log('Connected to Arduino');
 
-    parser = port.pipe(new ReadlineParser({ delimiter: '\n' }));
+    parser = port1.pipe(new ReadlineParser({ delimiter: '\n' }));
+
+    parser.on('data', data => {
+        console.log('Received from Arduino:', data.trim());
+    });
+});
+port2.on('open', () => {
+    isConnected = true;
+    console.log('Connected to Arduino');
+
+    parser = port2.pipe(new ReadlineParser({ delimiter: '\n' }));
 
     parser.on('data', data => {
         console.log('Received from Arduino:', data.trim());
     });
 });
 
-port.on('error', (err) => {
+port1.on('error', (err) => {
+    console.error('Error connecting to Arduino:', err.message);
+});
+port2.on('error', (err) => {
     console.error('Error connecting to Arduino:', err.message);
 });
 
 // Exported function: just writes to the existing port
 function sendToArduino(command) {
-    if (!isConnected || !port.writable) {
-        return console.error('Port not ready');
-    }
 
-    console.log(command);
-
-    port.write(command + '\n', (err) => {
-        if (err) {
-            console.error('Error writing to Arduino:', err.message);
-        } else {
-            console.log('Command sent to Arduino:', command);
+    if (command === '1') {
+        if (!isConnected || !port1.writable) {
+            return console.error('port1 not ready');
         }
-    });
+
+        port1.write(command + '\n', (err) => {
+            if (err) {
+                console.error('Error writing to Arduino:', err.message);
+            } else {
+                console.log('Command sent to Arduino:', command);
+            }
+        });
+    }
+    if (command === '2') {
+        if (!isConnected || !port2.writable) {
+            return console.error('port2 not ready');
+        }
+
+        port2.write(command + '\n', (err) => {
+            if (err) {
+                console.error('Error writing to Arduino:', err.message);
+            } else {
+                console.log('Command sent to Arduino:', command);
+            }
+        });
+    }
 }
 
 module.exports = {
