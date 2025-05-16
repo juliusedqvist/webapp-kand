d75ad019c61b843c02d3ea20601ff1346c3da76a
Julius Edqvist <jedqvist09@gmail.com>
2025-05-16 13:14:44 +0200

    command list

--- backend/routes/arduino.js
+++ backend/routes/arduino.js
@@ -3,6 +3,11 @@ const express = require('express');
 const router = express.Router();
 const { sendToArduino } = require('../services/arduinoController');
 
+const position_reference = {
+  B1_pickup: 10000,
+  RESET: "RESET"
+}
+
 router.post('/command', async (req, res) => {
   const { id, command } = req.body;
 
@@ -11,7 +16,8 @@ router.post('/command', async (req, res) => {
   }
 
   try {
-    const response = await sendToArduino(1, 10000);
+    console.log(position_reference[command]);
+    const response = await sendToArduino(0, position_reference[command]);
     res.json({ status: 'success', sent: command, arduinoResponse: response });
   } catch (err) {
     res.status(500).json({ error: 'Failed to send command', details: err.message });
