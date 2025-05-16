a8c728c0bd07dac47d2d7d138aef0d2ea85d4cd5
Julius Edqvist <jedqvist09@gmail.com>
2025-05-16 12:38:18 +0200

    fixes

--- backend/routes/arduino.js
+++ backend/routes/arduino.js
@@ -3,21 +3,6 @@ const express = require('express');
 const router = express.Router();
 const { sendToArduino } = require('../services/arduinoController');
 
-router.get('/test', (req, res) => {
-  res.send('Hello, world!');
-});
-
-// e.g., POST /api/arduino/led/on
-router.post('/led/:state', (req, res) => {
-  const state = req.params.state;
-  if (state !== 'on' && state !== 'off') {
-    return res.status(400).send('Invalid state');
-  }
-
-  sendToArduino(state === 'on' ? 'LED_ON' : 'LED_OFF');
-  res.json({ status: 'success', command: state });
-});
-
 router.post('/command', (req, res) => {
   const { command } = req.body;
 
