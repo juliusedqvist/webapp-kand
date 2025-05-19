7711aa0c0774a30aaffebd5577bca6018e22d624
Julius Edqvist <jedqvist09@gmail.com>
2025-05-19 12:25:00 +0200

    added a1

--- backend/routes/arduino.js
+++ backend/routes/arduino.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const { sendToArduino } = require('../services/arduinoController');
 
 const position_reference = {
-  B1_pickup: [[10000, 1], [5000, 0], ["RESET", 1]],
-  A1_leave: [[20000, 1], [5000, 0], ["RESET", 1]],
+  B1_pickup: [[10000, 1], [5000, 0], ["RESET", 0]],
+  A1_leave: [[20000, 1], [5000, 0], ["RESET", 0]],
   RESET: [["RESET", 0], ["RESET", 1]],
   STOP: "STOP",
   RESUME: "RESUME"
