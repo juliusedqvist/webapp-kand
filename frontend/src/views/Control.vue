db783aa25e05f360f0497f2623e86c56eb8a9a5a
Filip <rixfm14@gmail.com>
2025-05-20 09:49:40 +0200

    pos button

--- frontend/src/views/Control.vue
+++ frontend/src/views/Control.vue
@@ -69,6 +69,7 @@
           <button class="panel-btn" style="background-color: #f9cb00; color: white;" @click="logCommand('RESET', false);sendCommand('RESET')">RESET</button>
           <button class="panel-btn" style="background-color: #4cd000; color: white;" @click="logCommand('RESUME', false);sendCommand('RESUME')">RESUME</button>
           <button class="panel-btn" style="background-color: #004073; color: white;" @click="runNextAction">RUN NEXT</button>
+          <button class="panel-btn" style="background-color: #004073; color: white;" @click="sendCommand('REQUEST_POS')">LOG POS</button>
         </div>
       </div>
       <div class="next-action-panel">
@@ -156,7 +157,9 @@ async function runNextAction() {
 // Reusable helper for sending commands
 async function sendCommand(command) {
   console.log("Sending command:", command);
-  await axios.post('http://localhost:3000/api/arduino/command', { command });
+  const res = await axios.post('http://localhost:3000/api/arduino/command', { command });
+  console.log("Response from backend:", res)
+  log(res, 'info')
 }
 
 
@@ -167,6 +170,7 @@ async function logCommand(command, sendCommand = true) {
         const res = await axios.post('http://localhost:3000/api/arduino/command', {
           command: nextAction.value
         });
+        console.log("Response from backend:", res)
 
       } catch (err) {
         const msg = err.response?.data?.error || err.message || 'Unknown error';
@@ -428,6 +432,10 @@ watch(
 
 .log-entry.warning {
   color: #b8860b;
+}
+
+.log-entry.response {
+  color: #068f11;
 }
 
 .log-entry.error {
