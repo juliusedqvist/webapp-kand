d1b6446bf8505ab36f3fd1ac4d5a437fa7d24365
Erik Österling <erik2@osterling.eu>
2025-05-20 10:06:35 +0200

    Theta

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -3,12 +3,12 @@
 
 
 
-#define DriveF 9
-#define DriveB 10
+#define DriveF 10
+#define DriveB 9
 #define Feedb1 2 //The motors channel B, I think. It is synchronius with channel Z
 #define Feedb2 5 //The motors channel A, I think
 #define FeedbVarv 3 //The motors channel Z
-#define FeedbHitEnd 4
+#define FeedbHitEnd A5
 
 
 
@@ -41,13 +41,12 @@ int savedMissionIndex = 0;
 
 float delayTime = 20.0;
 
-
-volatile long locationNumber = 0; //Goes from 0 to somewhere between 10k and 20k
+volatile long locationNumber = 0; //Goes from 0 to like, 40-50k or smth
 long prevLocationNumber = locationNumber;
-long longagoPositionOne = locationNumber; //0 to 1.5s ago
-long longagoPositionTwo = locationNumber; //1.5 to 3s ago
-long longagoPositionThree = locationNumber; //3 to 4.5s ago
-int loopsPerLongagoPositionUpdate = 1500/delayTime;
+long longagoPositionOne = locationNumber; //0 to 2s ago
+long longagoPositionTwo = locationNumber; //2 to 4s ago
+long longagoPositionThree = locationNumber; //4 to 6s ago
+int loopsPerLongagoPositionUpdate = 2000/delayTime;
 int counter = 0;
 
 
@@ -58,16 +57,12 @@ long targetLocationNumber = 0;
 
 
 
-
-
-
-
-float P = 0.00035;
-float I = 0.000000005;
-float D = 0.05;
-float generalSpeedFactor = 0.45;
-int forwardsMargin = 25;
-int backwardsMargin = 5;
+float P = 0.0003;
+float I = 0;//.000000005;//.00000025;
+float D = 0.085;
+float generalSpeedFactor = 0.8;
+int forwardsMargin = 10;
+int backwardsMargin = 10;
 
 
 
@@ -94,10 +89,12 @@ void setup() {
   pinMode(Feedb2, INPUT);
   pinMode(FeedbVarv, INPUT);
   pinMode(FeedbHitEnd, INPUT_PULLUP);
+  //pinMode(FeedbHitEnd, INPUT);
 
 
 
 
+  pinMode(4, INPUT); //Just for safety - voltages will be applied to these, but will not be used.
   pinMode(6, INPUT); //Just for safety - voltages will be applied to these, but will not be used.
   pinMode(7, INPUT); //Just for safety - voltages will be applied to these, but will not be used.
   pinMode(13, OUTPUT); //Permanently high
@@ -149,6 +146,7 @@ void loop() {
       }
 
 
+
       incomingCommand = ""; // Reset buffer
     } else {
       incomingCommand += received;
@@ -167,7 +165,6 @@ void loop() {
 
 
   float speedNDir = 0;
-  
 	if(missionIndex == 1){
 	  float e = targetLocationNumber - locationNumber;
 	  float derivative = (locationNumber - prevLocationNumber)/delayTime;
@@ -206,15 +203,8 @@ void loop() {
 		Serial.println("done");
 	  }
 	 
-	  //If we are stuck against something but very close to target location, react quickly:
-	  if((abs(locationNumber - targetLocationNumber) < 100 && abs(locationNumber-longagoPositionTwo) < 25)){
-		missionIndex = 0;
-		savedMissionIndex = 0;
-		Serial.println("done");
-	  }
-	 
 	  //If we are stuck against something and not close to the target location, react slowly. Max allowed location diff is high to account for that the programs believed position often drifts when the robot is pushing against something it cant move.
-	  if((antistuckCurrentPWMBonus > 0.8 && abs(locationNumber-longagoPositionThree) < 150)){
+	  if((antistuckCurrentPWMBonus >= 1 && abs(locationNumber-longagoPositionThree) < 100)){
 		missionIndex = 0;
 		savedMissionIndex = 0;
 		Serial.println("fuck");
@@ -234,8 +224,11 @@ void loop() {
 
 
 	} else if(missionIndex == 2){
-	  if(digitalRead(FeedbHitEnd) == LOW){
+	  if(analogRead(FeedbHitEnd) > 50){ //1023 is 5V
 		missionIndex = 0;
+		//targetLocationNumber = 5000;
+		
+		
 		savedMissionIndex = 0;
 		locationNumber = 0;
 		integral = 0;
@@ -246,12 +239,9 @@ void loop() {
 		Serial.println("done");
         speedNDir = 0;
 	  } else{
-		speedNDir = -0.6;
+		speedNDir = -0.7;
 	  }
 	}
-
- 
-
 
 
 
@@ -286,9 +276,9 @@ void loop() {
 
 void Feedb1INTERRUPT(){
   if(digitalRead(Feedb2) == HIGH){
-    locationNumber += 1;
-  } else{
     locationNumber -= 1;
+  } else{
+    locationNumber += 1;
   }
 }
 
@@ -296,5 +286,4 @@ void Feedb1INTERRUPT(){
 void FeedbVarvINTERRUPT(){
   locationNumber = round(locationNumber/1000.0)*1000.0;
 }
-
 
