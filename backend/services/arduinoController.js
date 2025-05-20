c15f921ebcedc1621072dfe517546f2f8e857156
Julius Edqvist <jedqvist09@gmail.com>
2025-05-20 13:54:39 +0200

    sdf

--- backend/services/arduinoController.js
+++ backend/services/arduinoController.js
@@ -52,7 +52,7 @@ function sendToArduino(id, command) {
       const trimmed = data.trim().toLowerCase();
       console.log(`Response from Arduino ID ${id}: ${trimmed}`);
 
-      if (trimmed === 'done' || trimmed === 'fuck') {
+      if (trimmed === 'done' || trimmed === 'fuck' || trimmed.contains("Current")) {
         parser.off('data', onData);
         resolve(trimmed);
       }
