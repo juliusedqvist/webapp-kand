3c73a06c4c46c7fbdf24aa7102485c3c896a48b3
Erik Österling <erik2@osterling.eu>
2025-05-22 11:21:32 +0200

    r

--- arduino/Different motors/arduino_Theta.ino
+++ arduino/Different motors/arduino_Theta.ino
@@ -75,7 +75,7 @@ int16_t movementDir = 0; //-1 for backwards, +1 for forwards, 0 for standing sti
 int16_t locationNumberPreviousVarvInterrupt = locationNumber;
 int16_t numberOfSusVarvInterrupts = 0;
 int16_t currentTravelDirectionTracker = 0;
-int16_t timeLastDirectionSwap = counter;
+int32_t timeLastDirectionSwap = counter;
 
 
 
