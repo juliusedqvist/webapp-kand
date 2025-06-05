506055dd3081317ec408e8f14657d5b0f9a6dbef
Filip <rixfm14@gmail.com>
2025-06-05 13:15:53 +0200

    Values

--- backend/routes/arduino.js
+++ backend/routes/arduino.js
@@ -6,7 +6,9 @@ const { sendBroadcastCommand, sendToArduino } = require('../services/arduinoCont
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
-// 0: r, 1: theta, 2: z
+// 0: r 
+// 1: theta 
+// 2: z
 const position_reference = {
   B1_pickup: [[7220, 1], [51000, 2], [3855, 0], [74000, 2], ["RESET", 0], ["RESET", 1], ["RESET", 0], ["RESET", 1]],
   B1_leave: [[7220, 1], [74000, 2], [3855, 0], [51000, 2]],
